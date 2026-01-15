dcfa1c5178b26eebbd8f99b18115d19a9ea77cd9
Drew Krehel <drewkrehel@gmail.com>
2026-01-15 02:55:44 +0000

    set up devise and user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
   # get("/your_first_screen", { :controller => "pages", :action => "first" })
+
+  root "home#index"
 end
