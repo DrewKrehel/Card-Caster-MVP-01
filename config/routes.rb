6829d2828fd782df00d9cf99e4433a4c749eb9b3
Drew Krehel <drewkrehel@gmail.com>
2026-01-19 20:43:23 +0000

    veified view files are good.

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,17 @@
 Rails.application.routes.draw do
-  resources :session_users
-  resources :game_sessions
+
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
   # get("/your_first_screen", { :controller => "pages", :action => "first" })
-  
+
   root "projects#index"
-  
+
   devise_for :users
 
-  resources :game_sessions, controller: "sessions"
+  resources :game_sessions do
+    resources :session_users, only: [:create, :update, :destroy]
+  end
   resources :projects
 
   get "/users" => "users#index", as: :users
   get "/users/:username" => "users#show", as: :user
- 
-
 end
-
