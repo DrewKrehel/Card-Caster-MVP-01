ec4c12731c5d54d2adee51e6730d3aa438131140
Drew Krehel <drewkrehel@gmail.com>
2026-01-26 18:52:02 +0000

    Authorizations seem to work for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,9 @@ class ApplicationController < ActionController::Base
 
   def authorize_card_zone!(card)
     session_user = card.game_session.session_users.find_by(user: current_user)
-
     raise ActiveRecord::RecordNotFound unless session_user
 
-    allowed =
-      session_user.host? ||
-      (session_user.player? && session_user.zone_name == card.zone_name)
-
-    unless allowed
+    unless session_user.can_interact_with_zone?(card.zone_name)
       redirect_back fallback_location: game_session_path(card.game_session),
                     alert: "You are not allowed to interact with cards in this zone."
     end
