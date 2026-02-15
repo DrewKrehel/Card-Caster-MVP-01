609aa3c6a747646599d6b8fb2b7bb7e2130182b3
Drew Krehel <drewkrehel@gmail.com>
2026-02-15 21:27:13 +0000

    I think thats enough semantic HTML

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
       :sign_up,
-      keys: [:username, :avatar_image],
+      keys: [:username, :avatar_image, :bio],
     )
 
     devise_parameter_sanitizer.permit(
       :account_update,
-      keys: [:username, :avatar_image],
+      keys: [:username, :avatar_image, :bio],
     )
   end
 
